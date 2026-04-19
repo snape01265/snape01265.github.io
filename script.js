b875902834ba42e25d15d61d8af8d9950c3420e0
Sunwoo Lee <swlee6507@gmail.com>
2026-04-20 00:41:50 +0900

    Added custom formatting & changed page directory paths.

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
-function showPage(pageId) 
-{
-    const pages = document.querySelectorAll('.page');
-    
-    pages.forEach(page => 
-    {
-        page.classList.remove('active');
-    });
+const routes = {
+    // Landing pages
+    '/' : 'pages/home.html',
+    '/projects' : 'pages/projects.html',
+    '/experience' : 'pages/experience.html',
 
-    const targetPage = document.getElementById(pageId);
-    
-    targetPage.classList.add('active');
+    // Individual project pages
+    '/projects/my-little-puppy' : 'pages/projects/my-little-puppy.html',
+    '/projects/soul-after' : 'pages/projects/soul-after.html',
+    '/projects/space-haste' : 'pages/projects/space-haste.html',
+};
+
+async function Router()
+{
+    let hash = window.location.hash.replace('#', '');
+
+    if( hash === '' )
+    {
+        hash = '/';
+    }
+
+    const routePath = routes[hash];
+    const appRoot = document.getElementById('app-root');
+
+    return LoadPage(appRoot, routePath);
 }
+
+async function LoadPage(appRoot, routePath)
+{
+    // If route is invalid, redirect to home
+    if( routePath === undefined )
+    {
+        window.location.hash = '#/';
+        return;
+    }
+
+    // If app root is not found, log an error and redirect to home
+    if( appRoot === null )
+    {
+        console.error("App root element not found");
+        window.location.hash = '#/';
+        return;
+    }
+
+    try
+    {
+        const response = await fetch(routePath);
+        const html = await response.text();
+
+        appRoot.innerHTML = html;
+    }
+    catch( error )
+    {
+        console.error("Failed to load the page:", error);
+        appRoot.innerHTML = "<h2>Error loading page.</h2>";
+    }
+}
+
+window.addEventListener('hashchange', Router);
+window.addEventListener('load', Router);
