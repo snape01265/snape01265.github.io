eae849bdadec20dc61a571bf08f682183e80cb7e
Sunwoo Lee <swlee6507@gmail.com>
2026-05-17 20:54:24 +0900

    added resources and data for projects tab

---
+++ scripts/projects.js
@@ -0,0 +1,47 @@
+// --------------------------------
+// Projects Logic & Data
+// --------------------------------
+
+const projectData = {
+    "climb" :
+        {
+            title : "CLIMB",
+            date : "2026.04",
+            tags : [ "C++", "Unreal Engine" ],
+            description : `
+            <p>C.L.I.M.B is an incremental 3D climbing game I am currently developing. My goal for this project is to learn how to create games using UE5 and C++.</p>
+            <figure class="desc-figure">
+                <img src="assets/projects/climb/01.jpg" alt="CLIMB Gameplay">
+                <figcaption>Prototype</figcaption>
+            </figure>
+            <p>Although the main objective of this project is to learn a new language and engine, I am enjoying having full authorship of the entire codebase and participating in game design of core gameplay mechanics.</p>
+        `
+        },
+    "my-little-puppy" :
+        {
+            title : "My Little Puppy",
+            date : "2023.05~2026.03",
+            tags : [ "C#", "Unity" ],
+            description : `
+        <p>My Little Puppy is </p>
+        `
+        },
+    "space-haste" :
+        {
+            title : "Space Haste",
+            date : "2023.03~2023.03",
+            tags : [ "C#", "Unity" ],
+            description : `
+        <p>Space Haste</p>
+        `
+        },
+    "soul-after" :
+        {
+            title : "Soul After",
+            date : "2021.08~2022.08",
+            tags : [ "C#", "Unity" ],
+            description : `
+        <p>This project was </p>
+        `
+        }
+};
